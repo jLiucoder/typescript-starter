4a7ee58f0dd5f37c784026dfef1bf299dd1522b1
jLiucoder <55926282+jLiucoder@users.noreply.github.com>
2023-08-01 17:47:20 -0400

    change

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,15 @@ import { AppModule } from './app.module';
 
 // set globalprefix with api, and tried using  interceptors to catch the timeout exception but no luck
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    // app.useGlobalInterceptors(new TimeoutInterceptor());
-    app.setGlobalPrefix('api');
-    await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      rejectUnauthorized: false, // Ignore certificate validation
+      // cert: fs.readFileSync('path/to/certificate.pem'),
+      // key: fs.readFileSync('path/to/private-key.pem'),
+    },
+  });
+  // app.useGlobalInterceptors(new TimeoutInterceptor());
+  app.setGlobalPrefix('api');
+  await app.listen(3000);
 }
 bootstrap();
