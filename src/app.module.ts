e781a2349286d593788284788d13508bbb8d7d17
jLiucoder <55926282+jLiucoder@users.noreply.github.com>
2023-08-01 17:07:32 -0400

    modifies

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,21 +4,32 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 import { Task } from './typeorm/Task';
-
+import * as dotenv from 'dotenv';
+dotenv.config();
 // imports TasksModule and TypeOrmModule(for database connection)
 @Module({
     imports: [
         TasksModule,
         TypeOrmModule.forRoot({
+            // type: 'postgres',
+            // host: 'dpg-cj40qpt9aq0e0q2hgj60-a',
+            // port: 5432,
+            // username: 'dbname_c7aw_user',
+            // password: 'gWh2LBXzeQpOs2gBEuK83cCCOi8vCUqf',
+            // database: 'dbname_c7aw',
+            // entities: [Task],
+            // synchronize: true,
+            // url:'postgres://dbname_c7aw_user:gWh2LBXzeQpOs2gBEuK83cCCOi8vCUqf@dpg-cj40qpt9aq0e0q2hgj60-a.oregon-postgres.render.com/dbname_c7aw',
+            // ssl: true,
             type: 'postgres',
-            host: 'dpg-cj40qpt9aq0e0q2hgj60-a',
-            port: 5432,
-            username: 'dbname_c7aw_user',
-            password: 'gWh2LBXzeQpOs2gBEuK83cCCOi8vCUqf',
-            database: 'dbname_c7aw',
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE,
             entities: [Task],
             synchronize: true,
-            url:'postgres://dbname_c7aw_user:gWh2LBXzeQpOs2gBEuK83cCCOi8vCUqf@dpg-cj40qpt9aq0e0q2hgj60-a.oregon-postgres.render.com/dbname_c7aw',
+            url:process.env.DB_URL,
             ssl: true,
         }),
     ],
