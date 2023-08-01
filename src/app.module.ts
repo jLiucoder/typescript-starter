037b63ec4cd51ebe16f13a4a8c55ed8b01be4932
jLiucoder <55926282+jLiucoder@users.noreply.github.com>
2023-08-01 17:11:51 -0400

    modifies

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,7 +23,7 @@ dotenv.config();
             // ssl: true,
             type: 'postgres',
             host: process.env.DB_HOST,
-            port: process.env.DB_PORT,
+            port: 5432,
             username: process.env.DB_USERNAME,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_DATABASE,
